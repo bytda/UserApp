87d8ec62bf88ec6dfe95e6a9b04c4a64c0d7b27d
bytda <bytdae@gmail.com>
2022-05-06 00:53:15 +0300

    1st commit

---
+++ UsersApp/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UsersApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Data data = new Data();
+        private void ButtonReg_Click(object sender, RoutedEventArgs e)
+        {
+            data.Log = BoxLogin.Text.ToLower().Trim();
+            data.Pas = BoxPassword.Text.Trim();
+            if (data.Log != "" & data.Pas != "")
+            {
+                MessageBox.Show($"Вы зарегистрировались \nВаш логин:{data.Log} Ваш пароль:{data.Pas}");
+            }
+            else MessageBox.Show("Введите данные");
+
+        }
+         class Data
+        {
+            private string log = "Undefined";
+            public string Log
+            {
+                set { log = value; }
+                get { return log; }
+
+            }
+            private string pas = "Undefined";
+            public string Pas
+            {
+                set { pas = value; }
+                get { return pas; }
+
+            }
+        }
+            private void ButtonAuth_Click(object sender, RoutedEventArgs e)
+            {
+                Window1 window1 = new Window1();
+            window1.UserLog = data.Log;
+            window1.UserPas = data.Pas;
+
+            window1.Show();
+             this.Close();
+            ;
+
+
+            }
+        
+        }
+    }
+
---
+++ UsersApp/Window1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UsersApp
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {   public string UserLog { get; set; }
+        public string UserPas { get; set; }
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        
+       
+        private void ButtonAuth_Click(object sender, RoutedEventArgs e)
+        {
+            string login = BoxLogin.Text.ToLower().Trim();
+            string password = BoxPassword.Text.Trim();
+            if (login == UserLog & password == UserPas)
+            {
+                MessageBox.Show("Вы авторизованы");
+            }
+            else MessageBox.Show("Неверные данные");
+        }
+    }
+}
